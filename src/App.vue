e7a3094f655b3dbf559fa783c36f1d1f576f015c
Tinashe Chitakunye <tinashec93@live.com>
2022-07-06 11:33:06 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TaskList from './components/TaskList.vue';
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TaskList />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        <h3 class="title is-2">My TaskList</h3>
+    </div>
+</template>
+
+<style>
+h3 {
+    font-family: 'Titillium Web', sans-serif;
+}
+</style>
