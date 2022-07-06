3b410453286a83363ac31189df01fd4148d39537
Tinashe Chitakunye <tinashec93@live.com>
2022-07-06 13:58:05 +0200

    rebased

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="taskItem">
+    <p>{{ taskTitle }} is <em> {{ isDone ? 'Done' : 'not done'}}</em> | <span class="icon"><i class="fas fa-pencil-alt"></i></span>  | <span class="icon"><i class="fas fa-trash"></i></span></p>
+</div>   
+</template>
+
+<script>
+export default {
+    props: ['taskId', 'taskTitle', 'taskComplete'],
+    data: () => ({
+        id: 1,
+        title: 'Learn Azure cloud',
+        isDone: false
+    }),
+}
+</script>
+
+<style>
+.fa-trash {
+    color: red;
+    cursor: pointer;
+}
+.fa-pencil-alt {
+    color: orange;
+    cursor: pointer;
+}
+</style>
